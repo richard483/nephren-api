89a145c8f07a2e6452484b963da0420c0cd5fe36
Richard William <richard.william483@gmail.com>
2023-03-24 00:13:29 +0700

    added version tag

--- src/main/java/com/example/metanephren/controller/AuthController.java
+++ src/main/java/com/example/metanephren/controller/AuthController.java
@@ -5,16 +5,23 @@ import com.example.metanephren.requests.RegisterRequestVo;
 import com.example.metanephren.responses.MetaNephrenBaseResponse;
 import com.example.metanephren.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("api")
 public class AuthController {
   private final AuthService authService;
+  @Value("${nephren-api-version}") private String appVersion;
+  @Value("${nephren-application-name}") private String appName;
+
 
   @Autowired
   public AuthController(AuthService authService) {
@@ -30,5 +37,13 @@ public class AuthController {
   public Mono<MetaNephrenBaseResponse<Object>> register(@RequestBody RegisterRequestVo request) {
 
     return authService.register(request);
+  }
+
+  @GetMapping("/version")
+  public Mono<MetaNephrenBaseResponse<Object>> version() {
+    return Mono.just(MetaNephrenBaseResponse.builder()
+        .body(Map.of("application", appName, "nephren-api version", appVersion))
+        .success(true)
+        .build());
   }
 }
