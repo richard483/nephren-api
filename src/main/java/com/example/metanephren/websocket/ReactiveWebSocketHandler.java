ab99c8657154c0c89173bf528d42142a97ea86be
Richard William <richard.william@gdn-commerce.com>
2023-04-07 22:19:28 +0700

    fixed returned session into a proper message json on websocket connection

--- src/main/java/com/example/metanephren/websocket/ReactiveWebSocketHandler.java
+++ src/main/java/com/example/metanephren/websocket/ReactiveWebSocketHandler.java
@@ -2,6 +2,9 @@ package com.example.metanephren.websocket;
 
 import com.example.metanephren.models.Message;
 import com.example.metanephren.services.kafka.KafkaConsumerServices;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
@@ -10,21 +13,29 @@ import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-
 @Component
+@Slf4j
 public class ReactiveWebSocketHandler implements WebSocketHandler {
-  private final Flux<Message> intervalFlux;
+  private final Flux<Message> messageFlux;
+
+  private final ObjectMapper objectMapper;
 
   @Autowired
-  public ReactiveWebSocketHandler(KafkaConsumerServices kafkaConsumerServices) {
-    intervalFlux = Flux.interval(Duration.ofMillis(100L))
-        .zipWith(kafkaConsumerServices.messageConsumer(), (aLong, message) -> message);
+  public ReactiveWebSocketHandler(KafkaConsumerServices kafkaConsumerServices,
+      ObjectMapper objectMapper) {
+    messageFlux = kafkaConsumerServices.messageConsumer();
+    this.objectMapper = objectMapper;
   }
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
-    return session.send(intervalFlux.map(message -> session.textMessage(String.valueOf(message))))
-        .and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
+    return session.send(messageFlux.map(message -> {
+      log.info("#ReactiveWebSocketHandler info : {}", message.getMessage());
+      try {
+        return session.textMessage(objectMapper.writeValueAsString(message));
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+    })).and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
   }
 }
